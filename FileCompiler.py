53c65b9a7cf0848db3c8449bf00624dc5c0be84c
adatix <aditya@adatix.com>
2018-06-06 02:24:20 +0530

    compiled and working first form

--- FileCompiler.py
+++ FileCompiler.py
@@ -1,3 +1,10 @@
 import csv
-with open('C:\TempDir\ericsson_utran_cm_locell_05062018.asv') as csvfile:
+
+with open('C:\TempDir\ericsson_utran_cm_locell_05062018.asv','r') as csvfile:
     readCSV=csv.reader(csvfile,delimiter='|')
+# for row in readCSV:
+# print(row[1],row[4],row[29])
+    with open('C:\TempDir\ElectricalAntennaTilt.csv','w') as outputcsvfile:
+        writeCSV=csv.writer(outputcsvfile,delimiter=',')
+        for row in readCSV:
+            writeCSV.writerow(row);
