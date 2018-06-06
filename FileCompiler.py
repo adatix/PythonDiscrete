9fcf75450d4d7258cf399dc45003389592a44110
adatix <31534457+adatix@users.noreply.github.com>
2018-06-06 15:44:41 +0530

    Update FileCompiler.py

--- FileCompiler.py
+++ FileCompiler.py
@@ -1,9 +1,11 @@
 import csv
-
-with open('C:\TempDir\ericsson_utran_cm_locell_05062018.asv','r') as csvfile:
+import os,glob
+path='C:\TempDir\\'
+os.chdir(path)
+filename= glob.glob('ericsson_utran_cm_locell*.asv');
+file=path+filename[0]
+with open(file,'r') as csvfile:
     readCSV=csv.reader(csvfile,delimiter='|')
-# for row in readCSV:
-# print(row[1],row[4],row[29])
     with open('C:\TempDir\ElectricalAntennaTilt.csv','w') as outputcsvfile:
         writeCSV=csv.writer(outputcsvfile,delimiter=',')
         for row in readCSV:
