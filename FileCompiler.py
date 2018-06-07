267fbbd01d5be5c1207afc67cc7d5f304609e082
adatix <31534457+adatix@users.noreply.github.com>
2018-06-07 16:08:04 +0530

    Update FileCompiler.py

--- FileCompiler.py
+++ FileCompiler.py
@@ -1,12 +1,16 @@
 import csv
 import os,glob
-path='C:\TempDir\\'
+path='C:\TempDir\LandingZone\\'
 os.chdir(path)
 filename= glob.glob('ericsson_utran_cm_locell*.asv');
 file=path+filename[0]
+outputfilename='ElectricalAntennaTilt.csv'
+outputpath='C:\TempDir\ExtData\\'
+outputfile=outputpath+outputfilename;
+
 with open(file,'r') as csvfile:
     readCSV=csv.reader(csvfile,delimiter='|')
-    with open('C:\TempDir\ElectricalAntennaTilt.csv','w') as outputcsvfile:
+    with open(outputfile,'w') as outputcsvfile:
         writeCSV=csv.writer(outputcsvfile,delimiter=',')
         for row in readCSV:
             writeCSV.writerow(row);
